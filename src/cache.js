69f5ff728638d8dc41c381498349fd815bb12f90
Paweł Chochura <pawel.chochura@g2a.local>
2020-05-13 19:23:57 +0200

    fdg

--- src/cache.js
+++ src/cache.js
@@ -1,8 +1,8 @@
 const gitRootDir = require('git-root-dir');
 const sh = require('shelljs');
 const fs = require('fs');
-const loaded = false;
 const cache = new Map();
+let loaded = false;
 let path = undefined;
 
 const computePath = async () => {
@@ -20,6 +20,12 @@ const computePath = async () => {
 	path = `${git}/.git/.itg.cache`;
 };
 
+const clearFile = async () => {
+	await computePath();
+
+	fs.writeFileSync(path, '');
+}
+
 const loadFile = async () => {
 	await computePath();
 
@@ -33,8 +39,19 @@ const loadFile = async () => {
 	// Load file contents and treat each line as a 'key:value' pair
 	const content = fs.readFileSync(path).toLocaleString();
 	content.split('\n').forEach((line) => {
+		if (!line) {
+			return;
+		}
+
 		const pair = line.split(':');
-		cache.set(pair[0], pair[1]);
+		if (pair[0] === '__UNTIL__') {
+			if (Date.now() - pair[1] >= 0) {
+				cache.clear();
+				clearFile();
+				return;
+			}
+		}
+		cache.set(Buffer.from(pair[0], 'base64').toString('utf-8'), pair[1]);
 	});
 };
 
@@ -42,24 +59,45 @@ const saveFile = async () => {
 	await computePath();
 
 	const data = [];
+	let until;
 
 	// Save cache entries to an array as 'key:value' pair and then join them with a newline character
 	cache.forEach((value, key) => {
-		data.push(`${key}:${JSON.stringify(value)}`);
+		if (key === '__UNTIL__') {
+			until = value;
+		} else {
+			data.push(`${Buffer.from(key).toString('base64')}:${value}`);
+		}
 	});
+
+	if (!until) {
+		const threeDays = 1000 * 60 * 60 * 24 * 3;
+		until = Date.now() + threeDays;
+	}
+
+	data.push(`__UNTIL__:${until}`);
+
 	fs.writeFileSync(path, data.join('\n'));
 };
 
 const get = async (key) => {
 	await loadFile();
 
-	return Buffer.from(cache.get(key), 'base64').toString('utf-8');
+	const value = cache.get(key);
+
+	if (!value) {
+		return;
+	}
+
+	return JSON.parse(Buffer.from(value, 'base64').toString('utf-8'));
 };
 
 const set = async (key, value) => {
-	cache.set(key, Buffer.from(value).toString('base64'));
+	cache.set(key, Buffer.from(JSON.stringify(value)).toString('base64'));
 
 	await	saveFile();
+
+	return value;
 };
 
 const has = async (key) => {
