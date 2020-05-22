16b98934965d980719a69ff7cad05f0912502447
Paweł Chochura <pchochura@gmail.com>
2020-05-22 17:50:29 +0200

    prettier

--- src/issue.js
+++ src/issue.js
@@ -1,282 +1,339 @@
-require('dotenv').config();
+require('./extensions');
 const utils = require('./utils');
-const sh = require('shelljs');
+const api = require('./api');
+const sh = require('./shell');
 
 const showHelp = () => {
-  sh.echo(
-    `
-    Creates an issue with the given name and assignes it to you.
-    Use this script instead of creating issues through the browser
-    because this way the issue will be marked with the correct label and appropriate branch will be created and linked.
-    After creating issue you will be switched to the newly created branch.
-    Keep in mind that this branch automatically will be pushed.
+	sh.echo(
+		`
+		Creates an issue with the given name and assignes it to you.
+		Use this script instead of creating issues through the browser
+		because this way the issue will be marked with the correct label and appropriate branch will be created and linked.
+		After creating issue you will be switched to the newly created branch.
+		Keep in mind that this branch automatically will be pushed.
 
-    Usage:  ${process.env.LIB_NAME} issue|i [-h] <name> [-b|-c <label>] [--from <issue|'master'>] [-d]
-          \t${process.env.LIB_NAME} issue|i open <issue>
-    Options:
-    \t-h, --help, -help, h, help, ?   displays this help message
-    \t-b, --bug                       sets 'bug' label to the newly created issue
-    \t-c, --custom <label>            sets the given label to the newly created issue. Label have to exist.
-    \t--from <issue number|'master'>  allows to choose a base branch by selecting base issue
-    \t-d, --detached                  allows to create an issue without switching to the created branch
-    \topen <issue number>             changes branch to the one associated with the given issue and assignes it to you
+		Usage:  itg [issue|i] [-h] <name> [-b|-c <label>] [--from <issue|'master'>] [-d]
+		        itg [issue|i] open <issue>
+		        itg [issue|i] close [<'wont-do'|'done'>]
+		Options:
+		  -h, --help, -help, h, help, ?   displays this help message
+		  -b, --bug                       sets 'bug' label to the newly created issue
+		  -c, --custom <label>            sets the given label to the newly created issue. Label have to exist.
+		  --from <issue number|'master'>  allows to choose a base branch by selecting base issue
+		  -d, --detached                  allows to create an issue without switching to the created branch
+		  open <issue number>             changes branch to the one associated with the given issue and assignes it to you
+		  close [<'wont-do'|'done'>]      closes current issue; adds a comment with 'wont-do' or 'done' (default) description accordingly 
+		                                    and switches you to a master branch; if you want to provide a custom comment pass it as a parameter
+		                                    instead of 'wont-do' or 'done'
+
+		Prefix 'issue|i' can be omitted.
   `.trimIndent(),
-  );
-  sh.exit(0);
+	);
+	sh.exit(0);
 };
 
 const parseArgs = (args) => {
-  const options = {};
-  for (let i = 0; i < args.length; i++) {
-    if (['-h', '--help', '-help', 'h', 'help', '?'].indexOf(args[i]) !== -1) {
-      showHelp();
-    } else if (['-b', '--bug'].indexOf(args[i]) !== -1) {
-      options.bug = true;
-    } else if (['-c', '--custom'].indexOf(args[i]) !== -1) {
-      options.custom = args[i + 1];
+	const options = {};
+	for (let i = 0; i < args.length; i++) {
+		if (['-h', '--help', '-help', 'h', 'help', '?'].indexOf(args[i]) !== -1) {
+			showHelp();
+		} else if (['-b', '--bug'].indexOf(args[i]) !== -1) {
+			options.bug = true;
+		} else if (['-c', '--custom'].indexOf(args[i]) !== -1) {
+			options.custom = args[i + 1];
 
-      // Skip checking 'custom' parameter
-      i++;
-    } else if (['--from'].indexOf(args[i]) !== -1) {
-      options.from = args[i + 1];
+			// Skip checking 'custom' parameter
+			i++;
+		} else if (['--from'].indexOf(args[i]) !== -1) {
+			options.from = args[i + 1];
 
-      // Skip checking 'from' parameter
-      i++;
-    } else if (['-d', '--detached'].indexOf(args[i]) !== -1) {
-      options.detached = true;
-    } else if (['open'].indexOf(args[i]) !== -1) {
-      options.open = args[i + 1];
+			// Skip checking 'from' parameter
+			i++;
+		} else if (['-d', '--detached'].indexOf(args[i]) !== -1) {
+			options.detached = true;
+		} else if (['open'].indexOf(args[i]) !== -1) {
+			options.open = args[i + 1];
 
-      // Skip checking 'open' parameter
-      i++;
-    } else {
-      // Add as not named option (such as issue title)
-      options.title = args[i];
-    }
-  }
+			// Skip checking 'open' parameter
+			i++;
+		} else if (['close'].indexOf(args[i]) !== -1) {
+			options.open = args[i + 1];
 
-  return validateOptions(options);
+			// Skip checking 'close' parameter
+			i++;
+		} else {
+			// Add as not named option (such as issue title)
+			options.title = args[i];
+		}
+	}
+
+	return validateOptions(options);
 };
 
-const validateOptions = (tempOptions) => {
-  const options = {};
+const validateOptions = async (tempOptions) => {
+	const options = {};
 
-  // Validate 'open'
-  if (tempOptions.hasOwnProperty('open')) {
-    if (!tempOptions.open) {
-      sh.echo('You have to pass an issue number to "open" option');
-      sh.exit(1);
-    }
+	// Validate 'open'
+	if (tempOptions.hasOwnProperty('open')) {
+		if (!tempOptions.open) {
+			sh.echo('You have to pass an issue number to "open" option');
+			sh.exit(1);
+		}
 
-    // User typed more options than just "open <issue>"
-    if (Object.keys(tempOptions).length > 1) {
-      sh.echo('You cannot use option "open" with other options');
-      sh.exit(1);
-    }
+		// User typed more options than just "open <issue>"
+		if (Object.keys(tempOptions).length > 1) {
+			sh.echo('You cannot use option "open" with other options');
+			sh.exit(1);
+		}
 
-    options.open = validateOpen(tempOptions.open);
+		options.open = await validateOpen(tempOptions.open);
 
-    // Return now to avoid checking other options
-    return options;
-  }
+		// Return now to avoid checking other options
+		return options;
+	}
 
-  // Validate title
-  if (!tempOptions.title) {
-    sh.echo('You have to pass an issue title as a parameter');
-    sh.exit(1);
-  }
+	// Validate 'close'
+	if (tempOptions.hasOwnProperty('close')) {
+		// User typed more options than just "close [<'wont-do'|'done'>]"
+		if (Object.keys(tempOptions).length > 1) {
+			sh.echo('You cannot use option "close" with other options');
+			sh.exit(1);
+		}
 
-  options.title = tempOptions.title;
+		options.close = await validateClose(tempOptions.close);
 
-  // Validate 'custom' (ignore 'bug' if 'custom' is valid)
-  if (tempOptions.hasOwnProperty('custom')) {
-    if (!tempOptions.custom) {
-      sh.echo('You have to pass a label to "--custom" option');
-      sh.exit(1);
-    }
+		// Return now to avoid checking other options
+		return options;
+	}
 
-    options.label = validateCustom(tempOptions.custom);
-  } else if (tempOptions.bug) {
-    options.label = 'bug';
-  } else {
-    options.label = 'feature';
-  }
+	// Validate title
+	if (!tempOptions.title) {
+		sh.echo('You have to pass an issue title as a parameter');
+		sh.exit(1);
+	}
 
-  // Validate 'from'
-  if (tempOptions.hasOwnProperty('from')) {
-    if (!tempOptions.from) {
-      sh.echo('You have to pass an issue number or "master" to "--from" option');
-      sh.exit(1);
-    }
+	options.title = tempOptions.title;
 
-    options.from = validateFrom(tempOptions.from);
-  } else {
-    options.from = 'master';
-  }
+	// Validate 'custom' (ignore 'bug' if 'custom' is valid)
+	if (tempOptions.hasOwnProperty('custom')) {
+		if (!tempOptions.custom) {
+			sh.echo('You have to pass a label to "--custom" option');
+			sh.exit(1);
+		}
 
-  // Validate 'detached'
-  if (tempOptions.detached) {
-    options.detached = true;
-  }
+		options.label = await validateCustom(tempOptions.custom);
+	} else if (tempOptions.bug) {
+		options.label = 'bug';
+	} else {
+		options.label = 'feature';
+	}
 
-  return options;
+	// Validate 'from'
+	if (tempOptions.hasOwnProperty('from')) {
+		if (!tempOptions.from) {
+			sh.echo(
+				'You have to pass an issue number or "master" to "--from" option',
+			);
+			sh.exit(1);
+		}
+
+		options.from = await validateFrom(tempOptions.from);
+	} else {
+		options.from = utils.getCurrentBranchName();
+	}
+
+	// Validate 'detached'
+	if (tempOptions.detached) {
+		options.detached = true;
+	}
+
+	return options;
 };
 
-const validateCustom = (custom) => {
-  if (!custom) {
-    sh.echo('You have to pass a label');
-    sh.exit(1);
-  }
+const validateCustom = async (custom) => {
+	if (!custom) {
+		sh.echo('You have to pass a label');
+		sh.exit(1);
+	}
 
-  if (['bug', 'feature'].indexOf(custom) !== -1) {
-    sh.echo(`Warning!
-      Passing "bug" or "feature" to the "custom" option slows down the process. 
-      Try using "bug" option and keep in mind - "feature" label is the default.
-    `.trimIndent(),
-    );
-  }
+	// Download labels from Github repo
+	const labels = (await api.getLabels()).map((label) => label.name);
 
-  // Download labels from Github repo
-  const labels = sh
-    .exec('hub issue labels | grep -F "\n"', { silent: true })
-    .split('\n');
-
-  if (labels.indexOf(custom) === -1) {
-    sh.echo(
-      `You have to provide a label from: [${labels.join(', ').slice(0, -2)}]`,
-    );
-    sh.exit(1);
-  }
-  return custom;
+	if (labels.indexOf(custom) === -1) {
+		sh.echo(`You have to provide a label from: [${labels.join(', ')}]`);
+		sh.exit(1);
+	}
+	return custom;
 };
 
-const validateFrom = (from) => {
-  if (from === 'master') {
-    return 'master';
-  }
+const validateFrom = async (from) => {
+	if (from === 'master') {
+		return 'master';
+	}
 
-  if (!utils.validateNumber(from)) {
-    sh.echo(
-      'Parameter passed to "--from" have to be an issue number or "master"',
-    );
-    sh.exit(1);
-  }
+	if (!utils.validateNumber(from)) {
+		sh.echo(
+			'Parameter passed to "--from" have to be an issue number or "master"',
+		);
+		sh.exit(1);
+	}
 
-  return getBranchNameFromNumber(from);
+	return utils.getBranchNameFromNumber(from);
 };
 
-const validateOpen = (open) => {
-  if (!utils.validateNumber(open)) {
-    sh.echo('Parameter passed to "open" have to be an issue number');
-    sh.exit(1);
-  }
+const validateOpen = async (open) => {
+	if (!utils.validateNumber(open)) {
+		sh.echo('Parameter passed to "open" have to be an issue number');
+		sh.exit(1);
+	}
 
-  return {
-    branch: getBranchNameFromNumber(open),
-    number: open,
-  };
+	const issue = await api.getIssue(open);
+
+	return {
+		branch: utils.getBranchName(issue.title, issue.number),
+		number: issue.number,
+		id: issue.id,
+	};
 };
 
-const getBranchNameFromNumber = (issueNumber) => {
-  const issueTitleCommand = sh.exec(
-    `hub issue show -f %t ${issueNumber} | grep -F ""`,
-    {
-      silent: true,
-    },
-  );
+const validateClose = async (close) => {
+	if (close === 'wont-do') {
+		return "Closing issue. It won't be done.";
+	}
 
-  if (issueTitleCommand.code !== 0) {
-    sh.echo(
-      `Something went wrong with downloading issue (#${issueNumber}) title`,
-    );
-    sh.exit(1);
-  }
-
-  return getBranchName(issueTitleCommand.stdout, issueNumber);
-};
-
-const getBranchName = (issueTitle, issueNumber) => {
-  return `${utils.slugify(issueTitle)}-i${issueNumber}`;
+	return close === 'done' || !close ? 'Closing issue. It has been done already.' : close;
 }
 
-const runCommands = (options) => {
-  if (options.open) {
-    runOpen(options.open);
-  }
+const runCommands = async (options) => {
+	if (options.open) {
+		await runOpen(options.open);
+		sh.exit(0);
+	}
 
-  // Creating an issue
-  sh.echo(
-    `Creating issue with name: ${options.title}, labeled: ${options.label}`,
-  );
+	if (options.close) {
+		await runClose(options.close);
+		sh.exit(0);
+	}
 
-  let assignee = '';
-  if (!options.detached) {
-    sh.echo('Assigning this issue to you');
+	// Creating an issue
+	sh.echo(
+		`Creating issue with name: "${options.title}", labeled: "${options.label}"`,
+	);
 
-    assignee = `-a ${utils.getUser()}`;
-  }
+	let assignee = undefined;
+	if (!options.detached) {
+		sh.echo('Assigning this issue to you');
 
-  const issueLink = sh.exec(
-    `hub issue create -l '${options.label}' -m '${options.title}' ${assignee} | grep -F ""`,
-    { silent: true }
-  ).stdout.trimEndline();
+		const user = await api.getUser();
+		assignee = user.id;
+	}
 
-  if (!issueLink) {
-    sh.echo('Something went wrong with creating issue');
-    sh.exit(1);
-  }
+	const issue = await api.createIssue(options.title, options.label, assignee);
 
-  const indexOfSlash = issueLink.lastIndexOf('/') + 1;
-  const issueNumber = issueLink.substring(indexOfSlash);
+	if (!issue) {
+		sh.echo('Something went wrong with creating issue');
+		sh.exit(1);
+	}
 
-  if (!utils.validateNumber(issueNumber)) {
-    sh.echo('Something went wrong with creating issue');
-    sh.exit(1);
-  }
+	sh.echo(`Created issue: #${issue.number}`);
 
-  // Creating a custom branch
-  sh.echo('Checking out your new branch');
-  const branchName = getBranchName(options.title, issueNumber);
-  sh.exec(`git push origin origin/${options.from}:refs/heads/${branchName}`, { silent: true });
+	// Creating a custom branch
+	const branchName = utils.getBranchName(options.title, issue.number);
 
-  if (!options.detached) {
-    sh.exec('git stash', { silent: true });
-    sh.exec(`git checkout ${branchName}`, { silent: true });
-    sh.exec('git stash pop', { silent: true });
-  }
+	sh.echo(`Creating branch "${branchName}" based on "${options.from}"`);
 
-  // Adding description to the issue
-  const branchLink = sh.exec('hub browse -u | grep -F ""').replace(/[a-z0-9-]+$/, branchName);
-  const description = `Associated branch: [${branchName}](${branchLink})`;
-  sh.exec(`hub issue update "${issueNumber}" -m "${options.title}" -m "${description}"`);
+	sh.exec(`git push origin origin/${options.from}:refs/heads/${branchName}`);
 
-  sh.exit(0);
+	if (!options.detached) {
+		sh.echo('Checking out created branch');
+
+		const hasUncommitedChanges = sh.exec('git status -s').trimEndline() != '';
+		if (hasUncommitedChanges) {
+			sh.exec('git stash');
+		}
+
+		sh.exec(`git checkout ${branchName}`);
+
+		if (hasUncommitedChanges) {
+			sh.exec('git stash pop');
+		}
+	}
+
+	// Adding description to the issue
+	const branchLink = await utils.getBranchLink(branchName);
+	const description = `Associated branch: [${branchName}](${branchLink})`;
+
+	const result = await api.updateIssue(issue.id, description);
+
+	if (!result) {
+		sh.echo('Something went wrong with setting issue description');
+		sh.exit(1);
+	}
+
+	sh.exit(0);
 };
 
-const runOpen = (open) => {
-  sh.echo('Checking out branch associated with the selected issue');
+const runOpen = async (open) => {
+	sh.echo(
+		`Checking out branch "${open.branch}" associated with issue #${open.number}`,
+	);
 
-  // Check if remote branch with this name exist
-  if (
-    sh.exec(`git ls-remote origin ${open.branch}`, { silent: true }).code !== 0
-  ) {
-    sh.echo(`Remote branch "${open.branch}" does not exist`);
-    sh.exit(1);
-  }
+	// Check if remote branch with this name exist
+	if (sh.exec(`git ls-remote origin ${open.branch}`).code !== 0) {
+		sh.echo(`Remote branch "${open.branch}" does not exist`);
+		sh.exit(1);
+	}
 
-  // Checkout the branch
-  sh.exec(`git checkout --track origin/${open.branch}`, { silent: true });
+	// Check if local branch with this name exist
+	if (
+		sh.exec(`git show-ref --verify --quiet refs/heads/${open.branch}`).code ===
+		0
+	) {
+		// Checkout local branch
+		if (sh.exec(`git checkout ${open.branch}`).code !== 0) {
+			sh.echo(
+				'We had a problem with checking out the branch. Maybe stash changes before trying again?',
+			);
+			sh.exit(1);
+		}
 
-  sh.echo('Assigning this issue to you');
-  sh.exec(`hub issue update ${open.number} -a ${utils.getUser()}`, { silent: true });
+		// Pull changes from remote
+		sh.exec(`git pull origin ${open.branch}`);
+	} else {
+		// Checkout and track remote branch
+		if (sh.exec(`git checkout --track origin/${open.branch}`).code !== 0) {
+			sh.echo(
+				'We had a problem with checking out the branch. Maybe stash changes before trying again?',
+			);
+			sh.exit(1);
+		}
+	}
 
-  sh.exit(0);
+	sh.echo(`Assigning issue #${open.number} to you`);
+	const user = await api.getUser();
+	await api.updateIssue(open.id, undefined, user.id);
+
+	sh.exit(0);
 };
 
-const issue = (args) => {
-  const options = parseArgs(args);
-  runCommands(options);
-  sh.echo(options);
+const runClose = async (close) => {
+	const issueNumber = utils.getCurrentIssueNumber();
+	sh.echo(`Closing issue #${issueNumber}; reason: ${close}`);
+	
+	const issue = api.getIssue(issueNumber);
+	await api.closeIssue(issue.id, close);
+
+	sh.echo('Checking out "master" branch');
+	sh.exec('git checkout master');
+
+	sh.echo('Pulling changes from "origin master" branch');
+	sh.exec('git pull origin master');
+
+	sh.exit(0);
+};
+
+const issue = async (args) => {
+	await runCommands(await parseArgs(args));
 };
 
 module.exports = issue;
--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,80 @@
-const sh = require('shelljs');
+require('./extensions');
+const sh = require('./shell');
 
 const slugify = (input) => {
-  return input
-    .toLowerCase()
-    .replace(/\n|\r|\r\n/g, '')
-    .replace(/'/g, '')
-    .replace(/[^a-zA-Z0-9-]+/g, '-')
-    .replace(/-+/g, '-')
-    .replace(/^-*|-*$/g, '');
+	return input
+		.toLowerCase()
+		.replace(/\n|\r|\r\n/g, '')
+		.replace(/'/g, '')
+		.replace(/[^a-zA-Z0-9-]+/g, '-')
+		.replace(/-+/g, '-')
+		.replace(/^-*|-*$/g, '');
 };
 
 const validateNumber = (number) => {
-  return number.match(/^\d+$/);
+	return number.match(/^\d+$/);
 };
 
-const printError = (text) => {
-  // Example
-  // sh.echo('-e', '\033[31mYou have to pass an issue number to "open" option\033[0m');
+const getBranchNameFromNumber = async (issueNumber) => {
+	const api = require('./api');
+	const issue = await api.getIssue(issueNumber);
+
+	if (!issue) {
+		sh.echo(`Something went wrong with getting issue title (#${issueNumber})`);
+		sh.exit(1);
+	}
+
+	return getBranchName(issue.title, issueNumber);
 };
 
-const getUser = () => {
-  return JSON.parse(sh.exec('hub api user | grep -F ""', { silent: true }))
-    .login;
+const getBranchName = (issueTitle, issueNumber) => {
+	return `${slugify(issueTitle)}-i${issueNumber}`;
 };
 
-String.prototype.trimIndent = function () {
-  return this.replace(/\n */g, '\n');
+const getCurrentBranchName = () => {
+	return sh.exec('git rev-parse --abbrev-ref HEAD').trimEndline();
 };
 
-String.prototype.trimEndline = function () {
-  return this.replace(/\n/, '');
-}
+const getCurrentIssueNumber = () => {
+	const branchName = getCurrentBranchName();
+	const indexOfI = branchName.lastIndexOf('i');
+	const number = branchName.substring(indexOfI + 1);
+
+	if (indexOfI === -1 || !validateNumber(number)) {
+		sh.echo(
+			`There are no associated issues with current branch "${branchName}"`,
+		);
+		sh.exit(1);
+	}
+
+	return number;
+};
+
+const getNumberFromLink = (link) => {
+	const indexOfSlash = link.lastIndexOf('/') + 1;
+	const number = link.substring(indexOfSlash);
+
+	if (!number || !validateNumber(number)) {
+		sh.echo(`Something went wrong with getting issue number from "${link}"`);
+		sh.exit(1);
+	}
+
+	return number;
+};
+
+const getBranchLink = async (branch) => {
+	const api = require('./api');
+	const repoLink = (await api.getRepo()).url;
+	return `${repoLink}/tree/${branch}`;
+};
 
 module.exports = {
-  slugify,
-  validateNumber,
-  getUser,
+	slugify,
+	validateNumber,
+	getBranchName,
+	getBranchNameFromNumber,
+	getCurrentBranchName,
+	getCurrentIssueNumber,
+	getNumberFromLink,
+	getBranchLink,
 };
